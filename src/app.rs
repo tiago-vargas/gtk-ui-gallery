900cda88dc1a29fb67ce0bcebfdada8f2733a969
Tiago Vargas <tiago.ce10@outlook.com>
2023-08-16 14:52:11 -0300

    Create and use template for code labels

--- src/app.rs
+++ src/app.rs
@@ -78,10 +78,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "large-title",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 1, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 1, 1, 1] = &CodeLabel {
                     set_label: Examples::LargeTitle.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Title 1
@@ -90,10 +89,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "title-1",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 2, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 2, 1, 1] = &CodeLabel {
                     set_label: Examples::Title1.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Title 2
@@ -102,10 +100,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "title-2",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 3, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 3, 1, 1] = &CodeLabel {
                     set_label: Examples::Title2.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Title 3
@@ -114,10 +111,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "title-3",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 4, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 4, 1, 1] = &CodeLabel {
                     set_label: Examples::Title3.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Title 4
@@ -126,10 +122,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "title-4",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 5, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 5, 1, 1] = &CodeLabel {
                     set_label: Examples::Title4.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Heading
@@ -138,10 +133,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "heading",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 6, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 6, 1, 1] = &CodeLabel {
                     set_label: Examples::Heading.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Body
@@ -150,10 +144,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "body",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 7, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 7, 1, 1] = &CodeLabel {
                     set_label: Examples::Body.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Caption Heading
@@ -162,10 +155,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "caption-heading",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 8, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 8, 1, 1] = &CodeLabel {
                     set_label: Examples::CaptionHeading.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Caption
@@ -174,10 +166,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "caption",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 9, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 9, 1, 1] = &CodeLabel {
                     set_label: Examples::Caption.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Monospace
@@ -186,10 +177,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "monospace",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 10, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 10, 1, 1] = &CodeLabel {
                     set_label: Examples::Monospace.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Numeric
@@ -198,10 +188,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "numeric",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 11, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 11, 1, 1] = &CodeLabel {
                     set_label: Examples::Numeric.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
 
                 // Dim Label
@@ -210,10 +199,9 @@ impl WidgetTemplate for StyleExamplesSection {
                     add_css_class: "dim-label",
                     set_halign: gtk::Align::Start,
                 },
-                attach[2, 12, 1, 1] = &gtk::Label {
+                #[template]
+                attach[2, 12, 1, 1] = &CodeLabel {
                     set_label: Examples::DimLabel.as_str(),
-                    add_css_class: "monospace",
-                    set_halign: gtk::Align::Start,
                 },
             },
         }
@@ -344,6 +332,16 @@ impl Examples {
     add_css_class: "dim-label",
     set_halign: gtk::Align::Start,
 }"#,
+        }
+    }
+}
+
+#[relm4::widget_template(pub(crate))]
+impl WidgetTemplate for CodeLabel {
+    view! {
+        gtk::Label {
+            add_css_class: "monospace",
+            set_halign: gtk::Align::Start,
         }
     }
 }
